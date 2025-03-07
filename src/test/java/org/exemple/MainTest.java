a57e5f51f76bcf73959d4ff0dedc8709a51820e5
Your Name <abderrahmanerrachidygmail.com>
2025-03-07 00:42:04 +0000

    Jenkinsfile updatef

--- src/test/java/org/exemple/MainTest.java
+++ src/test/java/org/exemple/MainTest.java
@@ -12,6 +12,6 @@ class MainTest {
         // vérifier quelque chose de significatif dans votre application.
         // Dans cet exemple, on vérifie simplement que 1 + 1 est égal à 2.
 
-        assertEquals(String.valueOf(2), 1 + 1, "1 + 1 should equal 2");
+        assertEquals(2, 1 + 1);
     }
 }
